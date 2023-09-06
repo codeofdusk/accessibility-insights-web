881ea542c08f0c82de862502b5f54834621633b1
Bill Dengler <codeofdusk@gmail.com>
2023-09-06 13:42:03 -0700

    Move out string

--- src/DetailsView/components/overview-content/overview-heading-intro-text.ts
+++ src/DetailsView/components/overview-content/overview-heading-intro-text.ts
@@ -1,9 +1,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-export const overviewHeadingIntroTextForAssessment = `This page contains a summary that indicates the progress of your assessment. An
+import { productName } from 'content/strings/application';
+import styles from './overview-heading.scss';
+
+export const overviewHeadingIntroTextForAssessment = `
+                <div className={styles.overviewHeadingContent}>
+                    This page contains a summary that indicates the progress of your assessment. An
                     assessment is a manual experience in which you navigate through a set of tests
                     that cover all WCAG 2.1 AA success criteria. Each test has one or more
-                    requirements that can be:`;
+                    requirements that can be:
+                    <ul>
+                        <li>Automated</li>
+                        <li>Assisted</li>
+                        <li>Manual</li>
+                    </ul>
+                    Where possible, {productName} "assists" the test process by generating a list of
+                    instances to evaluate and highlighting them on the screen. {productName} also
+                    allows you to manually record failure instances.
+                </div>
+`;
 
-export const overviewHeadingIntroTextForQuickAssess = `Each test has one or more requirements that can be:`;
+export const overviewHeadingIntroTextForQuickAssess = ``;
--- src/DetailsView/components/overview-content/overview-heading.tsx
+++ src/DetailsView/components/overview-content/overview-heading.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { productName } from 'content/strings/application';
 import * as React from 'react';
 
 import { NamedFC } from '../../../common/react/named-fc';
@@ -20,17 +19,7 @@ export const OverviewHeading = NamedFC('OverviewHeading', (props: OverviewHeadin
                 data-automation-id={overviewHeadingAutomationId}
             >
                 <h1>Overview</h1>
-                <div className={styles.overviewHeadingContent}>
-                    {props.introText}
-                    <ul>
-                        <li>Automated</li>
-                        <li>Assisted</li>
-                        <li>Manual</li>
-                    </ul>
-                    Where possible, {productName} "assists" the test process by generating a list of
-                    instances to evaluate and highlighting them on the screen. {productName} also
-                    allows you to manually record failure instances.
-                </div>
+                {props.introText}
             </div>
         </>
     );
